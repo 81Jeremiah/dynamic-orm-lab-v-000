d27ea2bbbf2859d9be072a1abf6ce237f779d11e
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-05-29 18:32:49 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -61,6 +61,7 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
-
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys[0]} = ?"
+    DB[:conn].execute(sql, attribute.values[0])
   end
 end
